f8a6d81b818ece3eb5037629380718531e1f6f0b
vkeshav123 <keshavverma146@gmail.com>
2020-10-10 21:41:48 +0530

    update

--- first_project/infographApp/views.py
+++ first_project/infographApp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello Keshav")
+    return render(request, 'signIn.html')
---
+++ first_project/templates/control.js
@@ -0,0 +1,8 @@
+$("#section-list a").on("click", function (e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
+$("#myList a").on("click", function (e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
