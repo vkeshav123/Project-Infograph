09d611a93d6954e837fdf88388ea08266bd83e7e
vkeshav123 <keshavverma146@gmail.com>
2020-10-11 22:02:31 +0530

    update

--- first_project/infographApp/views.py
+++ first_project/infographApp/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'signIn.html')
+
+
+def moosa(request):
+    print(request.GET.get('email', 'No email provided'))
+    print(request.GET.get('password', 'No password provided'))
+    return HttpResponse("You have submitted credentials")
