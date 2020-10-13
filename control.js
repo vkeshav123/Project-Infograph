5e49808b52510d7040618b7d3f778fc121f09d73
vkeshav123 <keshavverma146@gmail.com>
2020-10-13 19:42:19 +0530

    remove trash

--- control.js
+++
@@ -1,8 +0,0 @@
-$("#section-list a").on("click", function (e) {
-  e.preventDefault();
-  $(this).tab("show");
-});
-$("#myList a").on("click", function (e) {
-  e.preventDefault();
-  $(this).tab("show");
-});
