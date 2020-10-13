1b31e1a737ceeb8357984f2aa381b6cfa62acc17
vkeshav123 <keshavverma146@gmail.com>
2020-10-13 20:35:11 +0530

    update

--- first_project/settings.py
+++ first_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static_files')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
--- infographApp/views.py
+++ infographApp/views.py
@@ -8,5 +8,9 @@ def index(request):
     return render(request, 'signIn.html')
 
 
+def signUpFunc(request):
+    return render(request, 'signUp.html')
+
+
 def info(request):
     return HttpResponse("This is information")
